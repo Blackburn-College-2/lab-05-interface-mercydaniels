5f9997552b13151161764cda5610a7ba2a34486a
mercydaniels <mercy.daniels@blackburn.edu>
2020-02-27 13:50:39 -0600

    still counting

--- src/emptyjavaproject/Walking.java
+++ src/emptyjavaproject/Walking.java
@@ -9,21 +9,43 @@ package emptyjavaproject;
  *
  * @author mercy.daniels
  */
-public class Walking implements CanTravel{
+public class Walking implements CanTravel {
+
     private final double mph = 5.5;
-   
+
     @Override
     public double move(double miles) {
-        double minutes = (miles/mph)/60;
+        double minutes = (miles / mph) / 60;
+        int hourCount = 0;
+        for (int i = 0; i <= minutes; i++) {
+            if (minutes > 60) {
+                hourCount++;
+                minutes = minutes - 60;
+            }
+        }
         System.out.println("Round Trip!");
         System.out.println("Leaving....");
-        System.out.println("It takes: " + minutes + " hours");
+        if (hourCount > 1) {
+            System.out.println("It takes: "
+                    + hourCount + " hours and  " + minutes + " minutes.");
+        } else if(hourCount == 1){
+            System.out.println("It takes: " + hourCount 
+                    + " hours and " + minutes + " minutes.");
+        }else{
+            if(minutes >= 1){
+                System.out.println("It takes: " +  minutes + " minute(s).");
+            } else{
+                System.out.println("");
+            }
+        }
+        System.out.println("Returning....");
         
+        return minutes;
     }
 
     @Override
     public double time() {
-        
+
     }
-    
+
 }
